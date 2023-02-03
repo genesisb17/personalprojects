4829a5975a6487ff889a024770a86b1ceb56d668
genesisb17 <genesis.bonds@gmail.com>
2023-02-03 10:02:03 -0500

    initial push for algorithm practice

---
+++ algorithms/sorting.js
@@ -0,0 +1,10 @@
+// Practice with different sorting algorithms in JS 
+
+let arr1 = [0, 3, 5, 6, 74, 32, -2, 325, 0];
+let arr2 = [-3, 5, 90, 5, 100, 3, 6, 7, 8, 9, 0, 1, -6, 34];
+
+
+function quicksort(){}
+
+
+function mergesort(){}
